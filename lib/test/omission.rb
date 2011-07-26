be7a9ea9756270c4f48fb351fc58679dd65f276f
7rans <transfire@gmail.com>
2011-07-26 14:24:00 -0400

    Remove Omission class, use NotImplementedError instead.

--- lib/test/omission.rb
+++
@@ -1,6 +0,0 @@
-# A test framework can raise an Omission error to mark a test
-# to be omitted. This may be done for a variety of reasons,
-# most commonly to mark a test pending completion.
-class Omission < Exception
-end
-
