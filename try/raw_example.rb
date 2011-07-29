fef8a4a306e4aaa840a69c5a8acf66e43362b75e
7rans <transfire@gmail.com>
2011-07-29 17:05:54 -0400

    Expand example to see all status types. [doc]

--- try/raw_example.rb
+++ try/raw_example.rb
@@ -1,7 +1,41 @@
-test = Object.new
-def test.call
+class RawTest
+  def initialize(label, &block)
+    @label = label
+    @block = block
+  end
+  def to_s
+    @label.to_s
+  end
+  def call
+    @block.call
+  end
+end
+
+# pass
+$TEST_SUITE << RawTest.new("all good")do
+end
+
+# fail
+$TEST_SUITE << RawTest.new("not so good")do
+  e = SyntaxError.new("failure is not an option")
+  e.set_assertion(true)
+  raise e
+end
+
+# error
+$TEST_SUITE << RawTest.new("not good at all") do
   raise "example error"
 end
 
-$TEST_SUITE << test
+# todo
+$TEST_SUITE << RawTest.new("it will be done") do
+  raise NotImplementedError, "but we're not there yet"
+end
+
+# omit
+$TEST_SUITE << RawTest.new("forget about it") do
+  e = NotImplementedError.new("it just can't be done")
+  e.set_assertion(true)
+  raise e
+end
 
