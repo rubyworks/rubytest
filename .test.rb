fc9e730fde9c6eaac42ee38499061776ca44de2c
7rans <transfire@gmail.com>
2011-07-28 23:41:52 -0400

    Use default rather than common. [admin]

--- .test.rb
+++ .test.rb
@@ -1,8 +1,9 @@
-Test.run(:common) do |run|
+Test.run(:default) do |run|
   run.files << 'test/*_case.rb'
 end
 
 Test.run(:cov) do |run|
+  run.files << 'test/*_case.rb'
   SimpleCov.start do |cov|
     cov.coverage_dir = 'log/coverage'
   end
