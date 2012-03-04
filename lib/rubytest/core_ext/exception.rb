b9dbf002dce1c6fc301daeacfd715fd1fd995651
7rans <transfire@gmail.com>
2012-03-04 13:05:19 -0500

    Fix check for Exception extension methods.

--- lib/rubytest/core_ext/exception.rb
+++ lib/rubytest/core_ext/exception.rb
@@ -2,18 +2,18 @@ class Exception
 
   def set_assertion(boolean)
     @assertion = boolean
-  end unless defined?(:set_assertion)
+  end unless method_defined?(:set_assertion)
 
   def assertion?
     @assertion
-  end unless defined?(:assertion?)
+  end unless method_defined?(:assertion?)
 
   def set_priority(integer)
     @priority = integer.to_i
-  end unless defined?(:set_priority)
+  end unless method_defined?(:set_priority)
 
   def priority
     @priority ||= 0
-  end unless defined?(:priority)
+  end unless method_defined?(:priority)
 
 end
