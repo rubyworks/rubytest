166b6a09f6f97835842cb8fd4c66e894e8c24872
7rans <transfire@gmail.com>
2013-02-10 09:04:20 -0500

    Fix autorunner.rb script. [bug]

--- lib/rubytest/autorun.rb
+++ lib/rubytest/autorun.rb
@@ -1,19 +1,7 @@
-#$TEST_SUITE = [] unless defined?($TEST_SUITE)
-
-if RUBY_VERSION < '1.9'
-  require 'rubytest'
-else
-  require_relative '../rubytest'
-end
+require 'rubytest'
 
 at_exit {
-  suite   = $TEST_SUITE
-  options = {
-    :format => ENV['rubytest-format']  # TODO: better name?
-  }
-
-  runner  = Ruth::Test::Runner.new(suite, options)
-  success = runner.run
+  success = Test.run
   exit -1 unless success
 }
 
