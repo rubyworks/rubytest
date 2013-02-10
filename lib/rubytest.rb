0bd4547d83cb9d3df03aa412ebb0db13ac3a1b1b
7rans <transfire@gmail.com>
2013-02-09 22:51:35 -0500

    Deprecate the use of rc gem.

--- lib/rubytest.rb
+++ lib/rubytest.rb
@@ -1,4 +1,4 @@
-# Ruby Test - a Universal Ruby Test Harness
+# Ruby Test - Universal Ruby Test Harness
 
 $TEST_SUITE          = [] unless defined?($TEST_SUITE)
 $RUBY_IGNORE_CALLERS = [] unless defined?($RUBY_IGNORE_CALLERS)
@@ -10,7 +10,6 @@ if RUBY_VERSION < '1.9'
   require 'rubytest/core_ext'
   require 'rubytest/code_snippet'
   require 'rubytest/config'
-  require 'rubytest/rc'
   require 'rubytest/recorder'
   require 'rubytest/advice'
   require 'rubytest/runner'
@@ -21,7 +20,6 @@ else
   require_relative 'rubytest/core_ext'
   require_relative 'rubytest/code_snippet'
   require_relative 'rubytest/config'
-  require_relative 'rubytest/rc'
   require_relative 'rubytest/recorder'
   require_relative 'rubytest/advice'
   require_relative 'rubytest/runner'
